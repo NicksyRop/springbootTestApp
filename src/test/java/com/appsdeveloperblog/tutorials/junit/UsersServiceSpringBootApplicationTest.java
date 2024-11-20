7f7ac35c773f5fb37dfbbd544aa11c26d5829205
Nickson <nnkipkorir@dtbafrica.com>
2024-11-20 11:00:49 +0300

    completed testing web layer

--- src/test/java/com/appsdeveloperblog/tutorials/junit/UsersServiceSpringBootApplicationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/UsersServiceSpringBootApplicationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
-@SpringBootTest
+@SpringBootTest //todo  - loads the entire application context,making it suitable for more comprehensive integration tests
 class UsersServiceSpringBootApplicationTest {
     @Test
     void contextLoads() {
