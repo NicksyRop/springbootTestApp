beed97bb593d6493ea30d4066ef2409bc936f17b
Nickson <nnkipkorir@dtbafrica.com>
2024-11-24 09:42:14 +0300

    test if custome findByUserId returns expected user

--- src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.tutorials.junit.io;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +17,24 @@ class UsersRepositoryTest {
     private TestEntityManager testEntityManager;
     @Autowired
     UsersRepository usersRepository;
+    UserEntity userEntity;
+
+    private final String userId = UUID.randomUUID().toString();
+
+    @BeforeEach
+    void setUp() {
+        userEntity = new UserEntity();
+        userEntity.setEmail("email@email.com");
+        userEntity.setFirstName("firstName");
+        userEntity.setLastName("lastName");
+        userEntity.setEncryptedPassword("encryptedPassword");
+        userEntity.setUserId(userId);
+    }
 
     @Test
     @DisplayName("Test findByEmail")
     void testFindByEmail_whenGivenCorrectEmail_returnUserEntity() {
         //arrange - create user entity and persist to users table
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("email@email.com");
-        userEntity.setFirstName("firstName");
-        userEntity.setLastName("lastName");
-        userEntity.setEncryptedPassword("encryptedPassword");
-        userEntity.setUserId(UUID.randomUUID().toString());
         testEntityManager.persistAndFlush(userEntity);
         //act
         UserEntity storedUser = usersRepository.findByEmail(userEntity.getEmail());
@@ -34,6 +42,20 @@ class UsersRepositoryTest {
         //assert
         assertEquals(userEntity.getEmail(), storedUser.getEmail(),
                 "Returned email address does not match expected email");
+    }
+
+    @Test
+    @DisplayName("Test findByUserId")
+    void testFindByUserId_whenGivenCorrectUserId_returnUserEntity() {
+        //arrange
+
+        //act
+        UserEntity createdUser = testEntityManager.persistAndFlush(userEntity);
+
+        //assert
+        assertNotNull(createdUser, "Created user should not be null");
+        assertEquals(userId, createdUser.getUserId(), "Returned user id does not match expected id");
+
     }
 
 }
